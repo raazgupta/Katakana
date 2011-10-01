8ecf51f8fb01a7b7511bb6c60f16fe59ceda07e0
Raj Gupta <raazgupta@gmail.com>
2011-10-01 19:09:59 +0900

    Katakana app using PhoneGap for iOS

---
+++ www/practiceList.js
@@ -0,0 +1,37 @@
+var practiceList = new Array();
+
+practiceList[0] = new Array();
+practiceList[0][0] = 'カ';
+practiceList[0][1] = 'ka';
+practiceList[0][2] = 'カナダ';
+practiceList[0][3] = 'ka na da';
+practiceList[0][4] = 'Canada';
+
+practiceList[1] = new Array();
+practiceList[1][0] = 'キ';
+practiceList[1][1] = 'ki';
+practiceList[1][2] = 'キャンプ';
+practiceList[1][3] = 'kya n pu';
+practiceList[1][4] = 'Camp';
+
+practiceList[2] = new Array();
+practiceList[2][0] = 'ク';
+practiceList[2][1] = 'ku';
+practiceList[2][2] = 'クイズ';
+practiceList[2][3] = 'ku i zu';
+practiceList[2][4] = 'Quiz';
+
+practiceList[3] = new Array();
+practiceList[3][0] = 'ケ';
+practiceList[3][1] = 'ke';
+practiceList[3][2] = 'ケニア';
+practiceList[3][3] = 'ke ni a';
+practiceList[3][4] = 'Kenia';
+
+practiceList[4] = new Array();
+practiceList[4][0] = 'コ';
+practiceList[4][1] = 'ko';
+practiceList[4][2] = 'コーヒー';
+practiceList[4][3] = 'koo hii';
+practiceList[4][4] = 'Coffee';
+
