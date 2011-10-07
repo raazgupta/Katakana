a4fe8be4861c281cf051e5780a028dd204e603a8
Raj Gupta <raazgupta@gmail.com>
2011-10-08 07:49:26 +0900

    Study page now updates entire array. Still need to add all the katakana.

--- www/practiceList.js
+++ www/practiceList.js
@@ -1,47 +1,93 @@
 var practiceList = new Array();
 
 practiceList[0] = new Array();
-practiceList[0][0] = 'カ';
-practiceList[0][1] = 'ka';
-practiceList[0][2] = 'カナダ';
-practiceList[0][3] = 'ka na da';
-practiceList[0][4] = 'Canada';
+practiceList[0][0] = 'ア';
+practiceList[0][1] = 'a';
+practiceList[0][2] = 'アメリカ';
+practiceList[0][3] = 'a me ri ka';
+practiceList[0][4] = 'America';
 practiceList[0][5] = 0;
 practiceList[0][6] = 4;
 
 practiceList[1] = new Array();
-practiceList[1][0] = 'キ';
-practiceList[1][1] = 'ki';
-practiceList[1][2] = 'キャンプ';
-practiceList[1][3] = 'kya n pu';
-practiceList[1][4] = 'Camp';
+practiceList[1][0] = 'イ';
+practiceList[1][1] = 'i';
+practiceList[1][2] = 'イギリス';
+practiceList[1][3] = 'i gi ri su';
+practiceList[1][4] = 'England';
 practiceList[1][5] = 0;
 practiceList[1][6] = 4;
 
 practiceList[2] = new Array();
-practiceList[2][0] = 'ク';
-practiceList[2][1] = 'ku';
-practiceList[2][2] = 'クイズ';
-practiceList[2][3] = 'ku i zu';
-practiceList[2][4] = 'Quiz';
+practiceList[2][0] = 'ウ';
+practiceList[2][1] = 'u';
+practiceList[2][2] = 'ウクライナ';
+practiceList[2][3] = 'u ku ra i na';
+practiceList[2][4] = 'Ukraine';
 practiceList[2][5] = 0;
 practiceList[2][6] = 4;
 
 practiceList[3] = new Array();
-practiceList[3][0] = 'ケ';
-practiceList[3][1] = 'ke';
-practiceList[3][2] = 'ケニア';
-practiceList[3][3] = 'ke ni a';
-practiceList[3][4] = 'Kenia';
+practiceList[3][0] = 'エ';
+practiceList[3][1] = 'e';
+practiceList[3][2] = 'エジプト';
+practiceList[3][3] = 'e ji pu to';
+practiceList[3][4] = 'Egypt';
 practiceList[3][5] = 0;
 practiceList[3][6] = 4;
 
 practiceList[4] = new Array();
-practiceList[4][0] = 'コ';
-practiceList[4][1] = 'ko';
-practiceList[4][2] = 'コーヒー';
-practiceList[4][3] = 'koo hii';
-practiceList[4][4] = 'Coffee';
+practiceList[4][0] = 'オ';
+practiceList[4][1] = 'o';
+practiceList[4][2] = 'オランダ';
+practiceList[4][3] = 'o la n da';
+practiceList[4][4] = 'Holland';
 practiceList[4][5] = 0;
 practiceList[4][6] = 4;
+
+
+practiceList[5] = new Array();
+practiceList[5][0] = 'カ';
+practiceList[5][1] = 'ka';
+practiceList[5][2] = 'カナダ';
+practiceList[5][3] = 'ka na da';
+practiceList[5][4] = 'Canada';
+practiceList[5][5] = 5;
+practiceList[5][6] = 9;
+
+practiceList[6] = new Array();
+practiceList[6][0] = 'キ';
+practiceList[6][1] = 'ki';
+practiceList[6][2] = 'キャンプ';
+practiceList[6][3] = 'kya n pu';
+practiceList[6][4] = 'Camp';
+practiceList[6][5] = 5;
+practiceList[6][6] = 9;
+
+practiceList[7] = new Array();
+practiceList[7][0] = 'ク';
+practiceList[7][1] = 'ku';
+practiceList[7][2] = 'クイズ';
+practiceList[7][3] = 'ku i zu';
+practiceList[7][4] = 'Quiz';
+practiceList[7][5] = 5;
+practiceList[7][6] = 9;
+
+practiceList[8] = new Array();
+practiceList[8][0] = 'ケ';
+practiceList[8][1] = 'ke';
+practiceList[8][2] = 'ケニア';
+practiceList[8][3] = 'ke ni a';
+practiceList[8][4] = 'Kenia';
+practiceList[8][5] = 5;
+practiceList[8][6] = 9;
+
+practiceList[9] = new Array();
+practiceList[9][0] = 'コ';
+practiceList[9][1] = 'ko';
+practiceList[9][2] = 'コーヒー';
+practiceList[9][3] = 'koo hii';
+practiceList[9][4] = 'Coffee';
+practiceList[9][5] = 5;
+practiceList[9][6] = 9;
 
